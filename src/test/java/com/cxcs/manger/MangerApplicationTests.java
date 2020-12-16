3fe855b5f2ac0ed0fea40a84febbecd62e357809
guyaren <1056760157@qq.com>
2020-12-16 23:54:56 +0800

    【describe】original commit 【developer】guyaren

---
+++ src/test/java/com/cxcs/manger/MangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxcs.manger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
